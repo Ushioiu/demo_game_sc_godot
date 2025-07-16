a5f772be1916e72503ceeb78605a4f684089d502
mingbc <uxy20020816@163.com>
2025-07-16 18:53:19 +0800

    铲球滑动

--- scenes/character_states/player_state_tackling.gd
+++ scenes/character_states/player_state_tackling.gd
@@ -1,13 +1,20 @@
 class_name PlayerStateTackling
 extends PlayerState
 
-const DURATION_TACKLE := 200
+const DURATION_FRIOR_RECOVERY := 200
+const GROUND_FRICATION := 250.0
 
-var time_start_tackle := Time.get_ticks_msec()
+var is_tackle_complete := false
+var time_finish_tackle := Time.get_ticks_msec()
 
 func _enter_tree() -> void:
 	animation_player.play("tackle")
 
-func _process(_delta: float) -> void:
-	if Time.get_ticks_msec() - DURATION_TACKLE > time_start_tackle:
-		state_transition_requested.emit(Player.State.RECOVERING)
+func _physics_process(delta: float) -> void:
+	if !is_tackle_complete:
+		player.velocity = player.velocity.move_toward(Vector2.ZERO, delta * GROUND_FRICATION)
+		if player.velocity == Vector2.ZERO:
+			is_tackle_complete = true
+			time_finish_tackle = Time.get_ticks_msec()
+	elif Time.get_ticks_msec() - time_finish_tackle > DURATION_FRIOR_RECOVERY:
+			state_transition_requested.emit(Player.State.RECOVERING)
